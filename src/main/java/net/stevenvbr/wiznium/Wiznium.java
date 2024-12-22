554e33b613708952918ecbfc815c9cb736f1b112
Stevenvbrr <steven.valentinfr88@gmail.com>
2024-12-22 15:57:45 +0100

    a

--- src/main/java/net/stevenvbr/wiznium/Wiznium.java
+++ src/main/java/net/stevenvbr/wiznium/Wiznium.java
@@ -1,6 +1,7 @@
 package net.stevenvbr.wiznium;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +14,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.stevenvbr.wiznium.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -31,6 +33,13 @@ public class Wiznium
         modEventBus.addListener(this::commonSetup);
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
+        ModItems.regiter(modEventBus);
+
+
+
+
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
@@ -45,7 +54,10 @@ public class Wiznium
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+    if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+        event.accept(ModItems.AZIUM_INGOT);
+        event.accept(ModItems.WIZNIUM_INGOT);
+    }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/stevenvbr/wiznium/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.stevenvbr.wiznium.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.stevenvbr.wiznium.Wiznium;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Wiznium.MOD_ID);
+
+    public static final RegistryObject<Item> AZIUM_INGOT = ITEMS.register("azium_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> WIZNIUM_INGOT = ITEMS.register("wiznium_ingot",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void regiter(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
